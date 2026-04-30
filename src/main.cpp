f375e48bdf7af429f1eb9936a4d0cb70b5df49d0
CCA8798 <2044187229@qq.com>
2026-04-30 23:54:37 +0800

    2026.04.30.23 First Commit
    1.创建了项目
    2.代码已在本地跑通

---
+++ src/main.cpp
@@ -0,0 +1,163 @@
+/*
+* CODE BY CCA8798, LAST EDIT IN 2026/4/30
+ * MIT LICENSE
+ */
+
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <vector>
+#include <random>
+#include "jsonxx/jsonxx.h"
+
+template<typename Ts=std::string>
+void pause(const Ts& text=std::string("")) {
+	if constexpr (std::is_convertible_v<Ts, std::string>) {
+		std::cout << text << std::endl;
+	} else {
+		throw std::invalid_argument("Invalid argument type for pause function. Expected a string or a type convertible to string!");
+	}
+	std::cin.get();
+	return;
+}
+
+std::string generateRandomString(const size_t len) {
+	static const char charset[] = "abcdefghijklmnopqrstuvwxyz0123456789";
+	static const size_t charsetSize = sizeof(charset) - 1;
+
+	static std::random_device randomDevice;
+	static std::mt19937 mt19937Generateor(randomDevice());
+	static std::uniform_int_distribution<size_t> dist(0, charsetSize - 1);
+
+	std::string result;
+	result.reserve(len);
+	for (size_t i = 0; i < len; ++i) {
+		result.push_back(charset[dist(mt19937Generateor)]);
+	}
+	return result;
+}
+
+int main() {
+	std::string currentFile("init");
+	while (true) {
+		std::cout<<"Please input the required file name in the new line, directly press enter to exit the program"<<std::endl;
+		std::getline(std::cin,currentFile);
+		if (currentFile.empty()) exit(0);
+		std::ifstream textData(currentFile);
+		if (!textData) {
+			std::cerr << "Error when opening the data file!";
+			throw std::runtime_error("Error when opening the data file!");
+		}
+
+		std::cout<<"Successfully read the data file! Parsing json text from the data file"<<std::endl;
+
+		jsonxx::Array jsonData;
+		std::stringstream stringStreamTemp;
+		stringStreamTemp<<textData.rdbuf();
+		if (!jsonData.parse(stringStreamTemp.str())) {
+			std::cerr << "Error parsing the data file!";
+			throw std::runtime_error("Error parsing the data file!");
+		}
+
+		std::vector<std::string> rollList={};
+		{
+			std::cout<<"==============Reads of files=============="<<std::endl;
+			std::cout<<"The users was read from the file:"<<std::endl;
+			for (size_t i = 0; i < jsonData.size(); ++i) {
+				std::string name = jsonData.get<std::string>(i);
+				rollList.emplace_back(name);
+				std::cout << name << std::endl;
+			}
+			std::cout<<std::endl;
+		}
+
+		/* CONFIG OF ROUNDS */
+		/* TODO: REMOVE THE GOTO FLAG IN THE FUTURE */
+		reinput:
+		size_t roundsCount=0;
+		size_t totalWinnersCount=0;
+		std::vector<size_t> resultsCountPerRound(roundsCount,0);
+		std::cout<<"==============Config of Rounds=============="<<std::endl;
+		{
+			std::cout<<"Please input the number of rounds: ";
+			std::cin>>roundsCount;
+			resultsCountPerRound.resize(roundsCount,0);
+			std::cout<<"Please input the number of winners per round in the next"<<std::endl;
+			for (size_t i = 0; i < roundsCount; ++i) {
+				std::cout<<"Winner number of Round "<<i+1<<": ";
+				std::cin>>resultsCountPerRound.at(i);
+				totalWinnersCount+=resultsCountPerRound.at(i);
+				if (totalWinnersCount > rollList.size()) {
+					std::cout<<"Sorry! The number of winners per round is larger than the number of users! Please Input Again!"<<std::endl;
+					goto reinput;
+				}
+			}
+		}
+
+		bool deleteUsersAfterRoll=false;
+		{
+			std::cout<<"Would you like to delete the winners from the list after the round? (Y/n): ";
+			char inputChar;
+			std::cin>>inputChar;
+			if (inputChar=='Y') {
+				deleteUsersAfterRoll = true;
+			}
+			std::cout<< std::endl;
+		}
+
+		bool rollsStepByStep=true;
+		{
+			std::cout<<"Would you like to roll step by step?(If no, the program will output all winners in once) (Y/n): ";
+			char inputChar;
+			std::cin>>inputChar;
+			if (inputChar=='n') {
+				rollsStepByStep = false;
+			}
+			std::cout<< std::endl;
+		}
+
+		pause("Press Any Key to start rolling from the showed list");
+
+		std::string randomString(generateRandomString(15));
+		{
+			namespace fs = std::filesystem;
+			if (!fs::exists("results")){
+				fs::create_directory("results");
+			}
+			if (!fs::exists("results/"+randomString)) {
+				fs::create_directory("results/"+randomString);
+			}
+		}
+
+		{
+			namespace fs=std::filesystem;
+			for (size_t r = 0; r < roundsCount; r++) {
+				for (size_t i = 0; i < resultsCountPerRound.at(r); i++) {
+					fs::path currentStageFolder="results/"+randomString+"/stage_"+std::to_string(r+1);
+					if (!fs::exists(currentStageFolder)) {
+						fs::create_directory(currentStageFolder);
+					}
+					jsonxx::Array currentRollJson;
+					for (auto& name: rollList) {
+						currentRollJson.append(name);
+					}
+					std::ofstream currentRollFile(currentStageFolder.string()+"/roll_"+std::to_string(i+1)+".json");
+					currentRollFile<<currentRollJson.json();
+					std::random_device randomDevice;
+					std::mt19937_64 mt19937Range(randomDevice());
+					std::uniform_int_distribution<size_t> dist(0, rollList.size() - 1);
+					size_t winner=dist(mt19937Range);
+					std::cout << "Winner "<<i+1<<" of Round "<<r+1<<" is "<< rollList.at(winner) << std::endl;
+					if (deleteUsersAfterRoll) {
+						rollList.erase(rollList.begin()+winner,rollList.begin()+winner+1);
+					}
+					if (rollsStepByStep) pause();
+				}
+			}
+		}
+
+		pause("Rolls Over! The id of the rolling round is"+randomString+", Press Any Key to start another round");
+		system("cls");
+	}
+	return 0;
+}
